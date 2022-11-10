b7cc8e4656ab50a7975fd8a7aef484e360e78f8a
Patrycja <pabramowska1@gmail.com>
2022-11-10 12:55:14 +0100

    Update

--- camera_pygame.py
+++ camera_pygame.py
@@ -1,19 +1,17 @@
 import pygame
 
-# pygame.init()
-#
+pygame.init()
 surface = pygame.display.set_mode((800,600))
 white = (255,255,255)
-#
-# color = (255,0,0)
-# pygame.draw.rect(surface, color, pygame.Rect(20,30,60,90))
 
-def cube(start, size):
+
+def cube(start, size_base, size_height):
+
     #pierwszy prostokąt
     point_1 = [start[0], start[1]]
-    point_2 = [start[0]+ size, start[1]]
-    point_3 = [start[0], start[1]+size]
-    point_4 = [start[0]+ size, start[1]+size]
+    point_2 = [start[0]+ size_base, start[1]]
+    point_3 = [start[0], start[1]+size_height]
+    point_4 = [start[0]+ size_base, start[1]+size_height]
 
     # linie łączące pierwszy prostokąt
     pygame.draw.line(surface, white, (point_1),(point_2))
@@ -22,11 +20,12 @@ def cube(start, size):
     pygame.draw.line(surface, white, (point_1),(point_3))
 
     #drugi prostokąt
-    shift = size//2
-    point_5 = [point_1[0] + shift, point_1[1] - shift]
-    point_6 = [point_2[0] + shift, point_2[1] - shift]
-    point_7 = [point_3[0] + shift, point_3[1] - shift]
-    point_8 = [point_4[0] + shift, point_4[1] - shift]
+    shift_base = size_base//2
+    shift_height = size_height//2
+    point_5 = [point_1[0] + shift_base, point_1[1] - shift_height]
+    point_6 = [point_2[0] + shift_base, point_2[1] - shift_height]
+    point_7 = [point_3[0] + shift_base, point_3[1] - shift_height]
+    point_8 = [point_4[0] + shift_base, point_4[1] - shift_height]
 
     #linie łączące pierwszy prostokąt
     pygame.draw.line(surface, white, (point_5),(point_6))
@@ -40,8 +39,17 @@ def cube(start, size):
     pygame.draw.line(surface, white, (point_3),(point_7))
     pygame.draw.line(surface, white, (point_4),(point_8))
 
-cube([250,225],100)
-cube([50,200],100)
+cube([250,225],70,100)
+cube([50,200],70,100)
 
-while True:
-    pygame.display.flip()
+game_on=True
+while game_on:
+    for event in pygame.event.get():
+        #sychodzenie z programu
+        if event.type == pygame.QUIT:
+            game_on = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                game_on = False
+
+    pygame.display.update()
